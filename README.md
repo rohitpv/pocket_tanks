23c92ff90e1842ff7bb800f90b31b53fa96c7847
P VENKATA ROHIT <pvrohit100@outlook.com>
2024-01-27 16:42:20 +0530

    File uploaded

---
+++ README.md
@@ -0,0 +1,59 @@
+#              POCKET TANKS mini
+
+**Simple pocket tanks console game made using c programming**
+
+**Graph plotting technique is used for displaying trajectory of weapon launched.**
+
+**delay is generated using nested for loop, for better and realistic viewing.**
+
+
+*****************************************************
+
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/M.png?raw=true)
+
+## **INSTRUCTIONS**
+
+_Enter -1 to move left...1 to move right...0 for no change_
+
+_USE 4  OR  6 KEYS TO CHANGE ANGLE_
+
+_USE 5 FOR TARGET LOCKING and AIM_
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/PT1.PNG?raw=true)
+
+**ENTERING PLAYER DETAILS**
+
+.
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/PT2.PNG?raw=true)
+
+**TO MOVE TANKER BY ONE STEP ENTER 1 OR -1 (RIGHT/LEFT)**
+
+.
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/PT3.PNG?raw=true)
+
+**TO CHANGE ANGLE ENTER 4 OR 6 (DECREASE/INCREASE)**
+
+.
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/PT4.PNG?raw=true)
+
+**LOCK TARGET BY PRESSING 5**
+
+.
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/PT5.PNG?raw=true)
+
+**SCORE CARD**
+
+.
+
+![POCKET TANKS](https://github.com/pvrohit100/pocket_tanks/blob/master/PT6.PNG?raw=true)
+
+**WINNER DETAILS**
+
+
+
+
---
+++ pocket_tanks.c
@@ -0,0 +1,164 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+void delay();
+int main()
+{
+int r,p1=0,p2=0,i,x,y,c1=0,c2=10,ch,p=0,q=3,z=0;
+char name1[10],name2[10];
+printf("\n            POCKET TANKS mini v2\n");
+printf("\n              INSTRUCTIONS  \n");
+printf("\n Enter -1 to go left...1 to go right...0 for no change \n");        //            printf("\n  \n");
+  printf("\n        USE 4  OR  6 KEYS TO CHANGE ANGLE \n");
+    printf("\n          USE 5 FOR TARGET LOCKING \n");
+printf("\n ENTER PLAYER 1 NAME \n");
+scanf("%s",name1);
+printf("\n ENTER PLAYER 2 NAME \n");
+scanf("%s",name2);
+system("clear");
+for(i=0;i<=5;i++)
+{
+	if(i%2==0)	//p1
+	{for(y=40;y>=0;y--)                  //disp
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                             if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else printf("  ");
+		          }printf("\n");
+			}
+	printf("\n %s its your turn \n Enter -1 to go left...1 to go right...0 for no change \n",name1);              //change position
+	scanf("%d",&ch);
+	if(ch==-1){--c1;}
+	else if(ch==1){++c1;}
+	else{}
+	for(y=40;y>=0;y--)                  //disp
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                             if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else printf("  ");
+		          }printf("\n");
+			}
+	p=0;
+	while(p!=5)
+	{printf("\nenter 4 / 6 for decreasing/increasing angle.....\n5 for target locking \n");
+	scanf("%d",&p);
+	if(p==4){--q;}
+	else if(p==6){++q;}
+	for(y=40;y>=0;y--)
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                            else if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else if(y==(c1-x)*(x-q)){ printf(" ."); }
+			     else printf("  ");
+		          }printf("\n");
+			}
+
+	}		//target locked
+	printf("\n target locked \n");
+for(r=1;r<=10;r++)
+{for(y=40;y>=0;y--)
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                            else if(y==0&&(x!=c1||x!=c2))   printf("__");
+                            else if((y==(c1-x)*(x-q))&&(x<=r)){ printf(" *"); }
+			     else printf("  ");
+		          }printf("\n");
+			}
+//system("clear");
+delay();
+}
+
+if((c1-c2)*(c2-q)==0){p1=p1+10;printf("\n good shot \n");}
+else if((c1-(c2-1))*((c2-1)-q)==0){p1=p1+5;printf("\n just missed.... good luck next time \n");}
+else if((c1-(c2+1))*((c2+1)-q)==0){p1=p1+5;printf("\n just missed.... good luck next time \n");}
+printf("\n %s                      %s\n ",name1,name2);
+printf("\n %d                          %d\n",p1,p2);
+
+	}		//p1 end
+
+
+
+
+
+	else   //p2
+	{
+	for(y=40;y>=0;y--)                  //disp
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                             if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else printf("  ");
+		          }printf("\n");
+			}
+	printf("\n %s its your turn \n Enter -1 to go left...1 to go right...0 for no change \n",name2);      //change position
+	scanf("%d",&ch);
+	if(ch==-1){--c2;}
+	else if(ch==1){++c2;}
+	else{}
+	for(y=40;y>=0;y--)                  //disp
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                             if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else printf("  ");
+		          }printf("\n");
+			}
+	z=0;
+	while(z!=5)
+	{printf("\nenter 4 / 6 for increasing/decreasing angle.....\n5 for target locking \n");
+	scanf("%d",&z);
+	if(z==4){--q;}
+	else if(z==6){++q;}
+	for(y=40;y>=0;y--)
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                            else if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else if(y==(c2-x)*(x-q)){ printf(" ."); }
+			     else printf("  ");
+		          }printf("\n");
+			}
+
+	}		//target locked
+	printf("\n target locked \n");
+for(r=10;r>0;r--)
+{
+for(y=40;y>=0;y--)
+			{for(x=-3;x<=13;x++)
+		         { if(y==0&&(x==c1||x==c2))  printf(" #");
+                            else if(y==0&&(x!=c1||x!=c2))   printf("__");
+                           else if((x>r)&&(y==(c2-x)*(x-q))){ printf(" *"); }
+			     else printf("  ");
+		          }printf("\n");
+			}
+delay();
+
+}
+
+if((c2-c1)*(c1-q)==0){p2=p2+10;printf("\n good shot \n");}
+else if((c2-(c1-1))*((c1-1)-q)==0) {p2=p2+5;printf("\n just missed.... good luck next time \n");}
+else if((c2-(c1+1))*((c1+1)-q)==0){p2=p2+5;printf("\n just missed.... good luck next time \n");}
+printf("\n %s                      %s\n ",name1,name2);
+printf("\n%d                          %d \n",p1,p2);
+
+
+
+
+	}
+}
+if(p1>p2)printf("\n %s wins\n",name1);
+else if(p1<p2)printf("\n %s wins\n",name2);
+else printf("\n DRAW MATCH\n");
+return 0;
+}
+void delay(){
+int i,j,s;
+float p,q;
+p=33445.44;
+q=845.5454;
+
+for(s=0;s<250;s++)
+{for(i=0;i<250;i++)
+{
+for(j=-100;j<100;j++){p=i*(float)i/39875*(1/998*p*49)*848*q*4/747;}
+}
+}
+
+}
